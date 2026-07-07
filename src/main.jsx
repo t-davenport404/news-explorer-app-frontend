fa0f434536a0d2748878b75fe4472e45424993c0
TonyD404 <ajd.davenport@gmail.com>
2026-07-06 22:38:06 -0400

    Chores: removed comments, cleaned syntax, deployed to Github.io

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createRoot } from "react-dom/client";
+import { createRoot, ReactDOM } from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
